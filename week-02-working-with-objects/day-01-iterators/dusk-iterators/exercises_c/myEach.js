e9706f5aafbfd5ece0ea370930af43f600925ecb
tgaff <tgaff@alumni.nd.edu>
2015-11-22 13:28:46 -0800

    Tests for myEach and solution

---
+++ week-02-working-with-objects/day-01-iterators/dusk-iterators/exercises_c/myEach.js
@@ -0,0 +1,22 @@
+/* write your myEach function body in the function below */
+
+function myEach(arr, callback) {
+
+//////////////////////////////////
+//  YOUR CODE HERE
+//////////////////////////////////
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// export this function (you can ignore this for now)
+module.exports = myEach;
