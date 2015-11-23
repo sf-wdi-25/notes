fe478f5a0aac26104d6152752c5ffe70d88afe71
tgaff <tgaff@alumni.nd.edu>
2015-11-22 16:52:26 -0800

    tests and solutions for myReduce

---
+++ week-02-working-with-objects/day-01-iterators/dusk-iterators/exercises_c/myReduce.js
@@ -0,0 +1,22 @@
+/* write your myReduce function body in the function below */
+
+function myReduce(arr, callback) {
+
+  //////////////////////////////////
+  //  YOUR CODE HERE
+  //////////////////////////////////
+
+
+}
+
+
+
+
+
+
+
+
+
+
+// export this function (you can ignore this for now)
+module.exports = myReduce;
